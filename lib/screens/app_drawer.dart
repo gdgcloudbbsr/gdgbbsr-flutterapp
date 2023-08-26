22f02decba30dc958c0d5e74509966781a7f3472
Swoyam Siddharth Nayak <b121065@iiit-bh.ac.in>
2023-08-27 00:28:12 +0530

    app working fine, notifications to be implemented

--- lib/screens/app_drawer.dart
+++ lib/screens/app_drawer.dart
@@ -14,58 +14,62 @@ class _AppDrawerState extends State<AppDrawer> {
   Widget build(BuildContext context) {
     return SafeArea(
       bottom: false,
-      child: FutureBuilder(
-        future: GdgApi.getAllSites(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Container(
-              height: double.infinity,
-              width: 300,
-              color: Colors.grey[900],
-              child: const Center(
-                child: CircularProgressIndicator(
-                  color: Colors.green,
+      child: SizedBox(
+        height: double.infinity,
+        width: 300,
+        child: FutureBuilder(
+          future: GdgApi.getAllSites(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Container(
+                height: double.infinity,
+                width: double.infinity,
+                color: Colors.grey[900],
+                child: const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.green,
+                  ),
                 ),
-              ),
-            );
-          } else {
-            return Container(
-              height: double.infinity,
-              width: 300,
-              color: Colors.grey[900],
-              child: ListView.builder(
-                itemCount: snapshot.data?['data'].length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: const Icon(
-                      Icons.web,
-                      color: Colors.white,
-                    ),
-                    title: Text(
-                      snapshot.data?["data"][index]["title"],
-                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                            color: Colors.white,
+              );
+            } else {
+              return Container(
+                height: double.infinity,
+                width: double.infinity,
+                color: Colors.grey[900],
+                child: ListView.builder(
+                  itemCount: snapshot.data?['data'].length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: const Icon(
+                        Icons.web,
+                        color: Colors.white,
+                      ),
+                      title: Text(
+                        snapshot.data?["data"][index]["title"],
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              color: Colors.white,
+                            ),
+                      ),
+                      onTap: () {
+                        Navigator.pop(context);
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomePage(
+                              pageName: snapshot.data?["data"][index]["name"],
+                              pageTitle: snapshot.data?["data"][index]["title"],
+                              pageLink: snapshot.data?["data"][index]["link"],
+                            ),
                           ),
-                    ),
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => HomePage(
-                            pageName: snapshot.data?["data"][index]["name"],
-                            pageTitle: snapshot.data?["data"][index]["title"],
-                            pageLink: snapshot.data?["data"][index]["link"],
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-            );
-          }
-        },
+                        );
+                      },
+                    );
+                  },
+                ),
+              );
+            }
+          },
+        ),
       ),
     );
   }
