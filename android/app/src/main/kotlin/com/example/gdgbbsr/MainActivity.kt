6273cb1d7eb8010f242a10bb856d6413ccf75272
Swoyam Siddharth Nayak <b121065@iiit-bh.ac.in>
2023-08-26 16:55:29 +0530

    app started

---
+++ android/app/src/main/kotlin/com/example/gdgbbsr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gdgbbsr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- gdg_bbsr/android/app/src/main/kotlin/com/example/gdg_bbsr/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gdg_bbsr
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
