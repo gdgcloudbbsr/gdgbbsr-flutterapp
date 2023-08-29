e604509b2e32eff82db0942008589bf893e3daad
Swoyam Siddharth Nayak <b121065@iiit-bh.ac.in>
2023-08-29 11:31:33 +0530

    Notifications working

--- lib/providers/api_calls.dart
+++ lib/providers/api_calls.dart
@@ -20,7 +20,22 @@ class GdgApi {
 
   // api call to get all sites
   static Future<Map<String, dynamic>> getAllSites() async {
-    final url = Uri.parse('https://gdgapp.swoyam.engineer/list/');
+    final url = Uri.parse('https://gdgapp.swoyam.engineer/sites/list/');
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      //parse response into map
+      Map<String, dynamic> result = jsonDecode(response.body);
+      return result;
+    } else {
+      return {'status': false};
+    }
+  }
+
+  // api call to get all sites
+  static Future<Map<String, dynamic>> getAllNotifications() async {
+    final url = Uri.parse('https://gdgapp.swoyam.engineer/notification/list');
 
     final response = await http.get(url);
 
--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../providers/api_calls.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
@@ -19,11 +21,60 @@ class _NotificationScreenState extends State<NotificationScreen> {
         centerTitle: true,
       ),
       backgroundColor: Colors.grey[900],
-      body: const Center(
-        child: Text(
-          'Notifications',
-          style: TextStyle(
-            color: Colors.white,
+      body: SafeArea(
+        bottom: false,
+        child: SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: FutureBuilder(
+            future: GdgApi.getAllNotifications(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  color: Colors.grey[900],
+                  child: const Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.green,
+                    ),
+                  ),
+                );
+              } else {
+                return Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  color: Colors.grey[900],
+                  child: ListView.builder(
+                    itemCount: snapshot.data?['data'].length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: const Icon(
+                          Icons.event,
+                          color: Colors.white,
+                        ),
+                        title: Text(
+                          snapshot.data?["data"][index]["title"],
+                          style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        subtitle: Text(
+                          snapshot.data?["data"][index]["description"],
+                          style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontWeight: FontWeight.normal,
+                          ),
+                          maxLines: 2,
+                        ),
+                        onTap: () {},
+                      );
+                    },
+                  ),
+                );
+              }
+            },
           ),
         ),
       ),
